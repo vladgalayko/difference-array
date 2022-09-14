54be2bee50448e830e0b261d03c55ee808bae113
Vlad Galayko <lorddarkvlad@gmail.com>
2022-09-14 12:23:00 +0300

    first commit

--- IntListsDifferenceSolution.js
+++ IntListsDifferenceSolution.js
@@ -1,13 +1,13 @@
 // 1)
-// const first = [1, 3, 3, 4, 6, 5, 4];
-// const second = [6, 3, 5, 2, 2];
+const first = [1, 3, 3, 4, 6, 5, 4];
+const second = [6, 3, 5, 2, 2];
 
-// let difference = first
-//                  .filter(x => !second.includes(x))
-//                  .concat(second.filter(x => !first.includes(x)));
+let difference = first
+                 .filter(x => !second.includes(x))
+                 .concat(second.filter(x => !first.includes(x)));
 
-// uniqArr = [...new Set(difference)].sort()                 
-// console.log(uniqArr)
+uniqArr = [...new Set(difference)].sort()                 
+console.log(uniqArr)
 
 // Implement a logic that finds difference between "first" and "secord" lists
 // and prints the result to the console:
@@ -15,21 +15,21 @@
 
 // 2)
 
-const first = [1, 3, 3, 4, 6, 5, 4];
-const second = [6, 3, 5, 2, 2];
+// const first = [1, 3, 3, 4, 6, 5, 4];
+// const second = [6, 3, 5, 2, 2];
 
-function difference(first, second) {
-    var result = [];
-    for (let i = 0; i < first.length; i++) {
-        for (let j = 0; j < second.length; j++) {
-            if (second.indexOf(first[i]) === -1) {
-                result.push(first[i]);
-            } else if (first.indexOf(second[j]) === -1) {
-                result.push(second[j]);
-            }
-        }
-    }
-    result = [...new Set(result)]
-    return result;
-  }
-console.log(difference(first, second))
+// function difference(first, second) {
+//     var result = [];
+//     for (let i = 0; i < first.length; i++) {
+//         for (let j = 0; j < second.length; j++) {
+//             if (second.indexOf(first[i]) === -1) {
+//                 result.push(first[i]);
+//             } else if (first.indexOf(second[j]) === -1) {
+//                 result.push(second[j]);
+//             }
+//         }
+//     }
+//     result = [...new Set(result)]
+//     return result;
+//   }
+// console.log(difference(first, second))
